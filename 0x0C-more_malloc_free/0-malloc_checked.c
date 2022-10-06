9fb16b41ba729abeb2ea36251ed5daeb6f138d71
mark8nigeria <favourekeleme58@gmail.com>
2022-10-06 11:49:31 -0700

    hello

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,18 +1,19 @@
 
-#include "main.h"
+#include <stdlib.h>
 /**
-* malloc_checked - function to allocate memory
-* @b: size of memeory
-*
-* Return: pointer to allocated memory
+* malloc_checked - multiplies two digits
+* @b: 1st digit in the multiplication
+* Description: multiplies two digits
+* Return: the result of the multiplication
 */
 void *malloc_checked(unsigned int b)
 {
-void *s;
-s = malloc(b);
-if (s == NULL)
+int *ptr;
+ptr = malloc(b);
+if (ptr == NULL)
 {
 exit(98);
+return (NULL);
 }
-return (s);
-} 
+return (ptr);
+}
